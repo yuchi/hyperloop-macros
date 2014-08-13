7308c956b0934e4bd72a362b65b9faef8f8bf83c
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-08-13 16:33:04 +0200

    almost working

--- objc.js
+++ objc.js
@@ -45,7 +45,7 @@ macroclass objc_method_params_alias_x {
 
 macroclass objc_method_params_alias {
   pattern {
-    rule { $part:ident $[:] $type:objc_type_name $arg:ident $inner:objc_method_params_alias }
+    rule { $part:ident $[:] $type:objc_type_name $arg:ident $inner:objc_method_params }
   }
   pattern {
     rule { $part:ident $[:] $type:objc_type_name $arg:ident }
@@ -55,12 +55,19 @@ macroclass objc_method_params_alias {
     rule { $part:ident }
     with $type = #{null}
     with $arg = #{null}
+    with $inner = #{null}
+  }
+}
+
+macro objc_method_params {
+  case { _ $params:objc_method_params_alias } => {
+    return #{ $params$part $params$type $params$arg $params$inner };
   }
 }
 
 macroclass objc_method_alias {
-  pattern { rule { + $returns:objc_type_name $params:objc_method_params_alias $body } with $static = #{1} }
-  pattern { rule { - $returns:objc_type_name $params:objc_method_params_alias $body } with $static = #{0} }
+  pattern { rule { + $returns:objc_type_name $params:objc_method_params $body } with $static = #{1} }
+  pattern { rule { - $returns:objc_type_name $params:objc_method_params $body } with $static = #{0} }
 }
 
 macro extract {
@@ -80,29 +87,43 @@ macro extract {
 }*/
 
 macro objc_method {
-  case { _ $m:objc_method_alias } => {
-    var name = unwrapSyntax(#{ $m$params$part });
+  case { $ctx $m:objc_method_alias } => {
+    var params = #{ $m$params }.slice(0, -1);
+    var name = unwrapSyntax(params[ 0 ]);
     var args = [];
-    var argPart = #{ $m$params$part };
-    var argType = #{ $m$params$type };
-    var argName = #{ $m$params$name };
-    var argInner = #{ $m$params$inner }
+    var argNames = [];
 
-    while (argType && argType[0] && argType[0].token.type !== parser.Token.NullLiteral) {
-      console.dir(argInner);
+    var i = 0, l = params.length;
+    var argPart, argType;
+    var ctx = #{ $ctx };
 
-      argType = null;
+    if (params[ 1 ].token.type !== parser.Token.NullLiteral) {
+      for (; i < l; i += 3) {
+        argPart = unwrapSyntax(params[ i + 0 ]);
+        argPart = makeValue(String(argPart), ctx);
+        argType = unwrapSyntax(params[ i + 1 ]);
+        argType = makeValue(String(argType), ctx);
+        argName = unwrapSyntax(params[ i + 2 ]);
+        argName = makeIdent(String(argName), ctx);
+
+        args = args.concat(#{,}).concat([ makeDelim( '{}', #{name:}.concat(argPart).concat(#{,}).concat(#{type:}).concat(argType), ctx) ]);
+        argNames = argNames.concat(#{,}).concat(argName);
+      }
+
+      args = args.slice(1);
+      argNames = argNames.slice(1);
     }
 
     letstx $methodName = [ makeValue(name, #{ $m$params$name }) ];
-    letstx $methodArgs = [ makeDelim("[]", args, #{ $m$params$name }) ]
+    letstx $methodArgs = args;
+    letstx $funcArgs = argNames;
 
     return #{{
         name: $methodName,
         static: $m$static,
         returns: $m$returns,
-        arguments: $methodArgs,
-        action: function () $m$body
+        arguments: [ $methodArgs ],
+        action: function ( $funcArgs ) $m$body
     }}
   }
 }
@@ -123,22 +144,22 @@ macro class {
 }
 
 class native GestureRecognizer {
-  + ((UIWindow*)*) gestureRecognize { return this; }
-  + ((UIWindow*)*) gestureRecognize:(id) gesture lol:(id)lol { return this; }
-  + (UIWindow*) gestureRecognize:(id) gesture { return this; }
-  + (UIWindow) gestureRecognize:(id) gesture { return this; }
-  + UIWindow gestureRecognize:(id) gesture { return this; }
-  - ((UIWindow*)*) gestureRecognize:(id) gesture { return this; }
-  - (UIWindow*) gestureRecognize:(id) gesture { return this; }
-  - (UIWindow) gestureRecognize:(id) gesture { return this; }
-  - UIWindow gestureRecognize:(id) gesture { return this; }
-  + "UIWindow**" gestureRecognize:(id) gesture { return this; }
-  + "UIWindow*" gestureRecognize:(id) gesture { return this; }
-  + "UIWindow" gestureRecognize:(id) gesture { return this; }
-  - "UIWindow**" gestureRecognize:(id) gesture { return this; }
-  - "UIWindow*" gestureRecognize:(id) gesture { return this; }
-  - "UIWindow" gestureRecognize:(id) gesture { return this; }
-  - void gestureRecognize:(id) gesture { return this; }
+  //+ ((UIWindow*)*) gestureRecognize { return this; }
+  + ((UIWindow*)*) gestureRecognize:(id)gesture lol:(id)x { return gesture + x; }
+  //+ (UIWindow*) gestureRecognize:(id) gesture { return this; }
+  //+ (UIWindow) gestureRecognize:(id) gesture { return this; }
+  //+ UIWindow gestureRecognize:(id) gesture { return this; }
+  //- ((UIWindow*)*) gestureRecognize:(id) gesture { return this; }
+  //- (UIWindow*) gestureRecognize:(id) gesture { return this; }
+  //- (UIWindow) gestureRecognize:(id) gesture { return this; }
+  //- UIWindow gestureRecognize:(id) gesture { return this; }
+  //+ "UIWindow**" gestureRecognize:(id) gesture { return this; }
+  //+ "UIWindow*" gestureRecognize:(id) gesture { return this; }
+  //+ "UIWindow" gestureRecognize:(id) gesture { return this; }
+  //- "UIWindow**" gestureRecognize:(id) gesture { return this; }
+  //- "UIWindow*" gestureRecognize:(id) gesture { return this; }
+  //- "UIWindow" gestureRecognize:(id) gesture { return this; }
+  //- void gestureRecognize:(id) gesture { return this; }
 };
 
 console.log( objc_type_name ((UIWindow*)*) )
